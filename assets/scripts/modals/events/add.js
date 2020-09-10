28ec657e3c33ce1b10c0c340523a80409a8b3a5a
karolinadablowska <karolina.dablowska@hotmail.com>
2020-09-10 19:53:39 +0200

    feat: modal add event

--- assets/scripts/modals/events/add.js
+++ assets/scripts/modals/events/add.js
@@ -1,9 +1,24 @@
+require("../../../styles/utils/components/modals/events/_add.scss");
+
 export default class {
 	
+	// konstruktor klasy
 	constructor() {
 		
-		console.log("modal add event works fine.");
+		this.onClickOverlay();
 		
+	}
+	
+	onClickOverlay() {
+		
+		$(".modal-add-event-overlay").click(function() {
+			
+			// główny kontener okna modalnego
+			var modal = $(this).closest(".modal-add-event");
+			
+			// ukrycie okna modalnego
+			modal.removeClass("modal-add-event-toggle")
+		});
 	}
 	
 }
