43eddbd44de459c8fa55c51399d471cb007df7f0
karolinadablowska <karolina.dablowska@hotmail.com>
2020-09-14 20:07:51 +0200

    feat: add event modal

--- assets/scripts/modals/events/add.js
+++ assets/scripts/modals/events/add.js
@@ -1,12 +1,20 @@
 require("../../../styles/utils/components/modals/events/_add.scss");
+require("../../../styles/utils/components/inputs/_input.scss");
+require("../../../styles/utils/components/textareas/_textarea.scss");
 
 export default class {
 	
 	// konstruktor klasy
 	constructor() {
 		
+		this.onCreateEvent();
 		this.onClickOverlay();
 		
+	}
+	
+	toggleModal() {
+		
+		$(".modal-add-event").toggleClass("modal-add-event-toggle");
 	}
 	
 	onClickOverlay() {
@@ -18,6 +26,20 @@ export default class {
 			
 			// ukrycie okna modalnego
 			modal.removeClass("modal-add-event-toggle")
+		});
+	}
+	
+	onCreateEvent() {
+		
+		$(".button-add-event-create").click(function() {
+			
+			console.log("tworzenie wydarzenia");
+			
+			// główny kontener okna modalnego
+			// var modal = $(this).closest(".modal-add-event");
+			
+			// ukrycie okna modalnego
+			// modal.removeClass("modal-add-event-toggle")
 		});
 	}
 	
--- assets/scripts/pages/dashboard.js
+++ assets/scripts/pages/dashboard.js
@@ -2,4 +2,31 @@ require("../../styles/pages/_dashboard.scss");
 
 import ModalAddEvent from "../modals/events/add.js";
 
-new ModalAddEvent();
+class DashboardPage {
+	
+	constructor() {
+		
+		this.modals = {
+			
+			"events": {
+				
+				"add": new ModalAddEvent()
+			}
+		};
+		
+		// events
+		this.onAddEvent();
+	}
+	
+	onAddEvent() {
+		
+		const self = this;
+		
+		$(".button-add-event").click(function() {
+			
+			self.modals.events.add.toggleModal();
+		});
+	}
+}
+
+new DashboardPage();
